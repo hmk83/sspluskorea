768b82a289288094d79d15b554f021a337418dbd
hmk83 <bluemusic001@gmail.com>
2026-05-15 09:07:45 +0900

    Delete 소유스캔 directory

--- 소유스캔/supabase-config.js
+++
@@ -1,8 +0,0 @@
-window.SOYU_SUPABASE_CONFIG = {
-  url: "https://imaoieqwteqwsssicuxr.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltYW9pZXF3dGVxd3Nzc2ljdXhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3Mzg5MjYsImV4cCI6MjA5NDMxNDkyNn0.neebqRFbkU-4HNKIP3VLUD5wpFmCPtLELz-PDCwh36M",
-  adminEmail: "admin@soyou-scan.local",
-  inquiryTable: "inquiries",
-  pageViewTable: "page_views",
-  trackEndpoint: "https://soyu-scan-track.sspluskorea-soyu-scan.workers.dev"
-};
